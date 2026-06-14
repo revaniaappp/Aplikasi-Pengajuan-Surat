f28294cb77982215992de73b935adc3b25d9b56b
Jesye Octavia Nainggolan <octaviajesye02@gmail.com>
2026-06-14 22:41:22 +0700

    Menambahkan button archive pengguna untuk admin set

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\ProgramStudi;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -19,5 +20,28 @@ class UserController extends Controller
         $users = $query->orderBy('name')->get();
 
         return view('admin.users.index', compact('users'));
+    }
+
+    public function edit($id)
+    {
+        $user = User::findOrFail($id); 
+        $prodis = ProgramStudi::all();
+        
+        return view('admin.users.edit', compact('user', 'prodis'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+        
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email,' . $id,
+            'role' => 'required',
+        ]);
+
+        $user->update($request->all());
+
+        return redirect()->route('admin.users.index')->with('success', 'Data pengguna berhasil diperbarui');
     }
 }
